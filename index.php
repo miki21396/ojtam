99a35c4991ba55e5af1dfc4e3eaacd788f86aa30
Miki Fukazawa <>
2022-11-14 00:09:45 +0900

    done wordpress

--- index.php
+++ index.php
@@ -1,91 +1,46 @@
-<?php get_header();?>
+<?php get_header(); ?>
 
-        <!-- MIKI -->
-        <p class="maintitle">Latest Articles</p>
-        <!-- main roop starts from here -->
+<p class="maintitle">Latest Articles</p>
+<div class="contentss">
+    <?php
+    if (have_posts()) :
+        while (have_posts()) :
+            the_post();
+    ?>
+            <div class="content-hover">
+                <div class="content">
+                    <img src="<?php the_post_thumbnail_url(); ?>">
+                    <div class="texts">
+                        <p class="day"><?php the_date(); ?></p>
+                        <p class="title"><?php the_title(); ?></p>
+                        <p class="more"><a href="" class="line"><span class="readmore" href="http://">READ MORE</p>
+                        </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
         <?php
-        if (have_posts()) :
-            while (have_posts()) :
-              the_post():
+        endwhile;
+    else :
         ?>
+
+<!-- コメントアウトするとページ上にー－＞が表示される -->
+
         <div class="contentss">
             <div class="content-hover">
                 <div class="content">
-                    <img src="<?php echo get_template_directory_uri();?>/images/post_img_1.png">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/post_img_1.png">
                     <div class="texts">
                         <p class="day">2018/5/20</p>
-                        <p class="title">おしゃれカフェがありますよ</p>
+                        <p class="title">unknown title</p>
                         <p class="more"><a href="" class="line"><span class="readmore" href="http://">READ MORE</p>
                         </span>
                         </a>
                     </div>
                 </div>
 
-                <?php 
-                    endwhile;
-                    else:
-                 ?>
-                 
+            <?php endif; ?>
+
             </div>
-            <div class="content-hover">
-                <div class="content">
-                    <img src="<?php echo get_template_directory_uri();?>/images/post_img_2.png">
-                    <div class="texts">
-                        <p class="day">2018/5/19</p>
-                        <p class="title">あのネオンはいつ交換するのか！？観覧車の謎に迫る！</p>
-                        <p class="more"><a href="" class="line"><span class="readmore" href="http://">READ MORE</p>
-                        </span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="content-hover">
-                <div class="content">
-                    <img src="<?php echo get_template_directory_uri();?>/images/post_img_3.png">
-                    <div class="texts">
-                        <p class="day">2018/5/18</p>
-                        <p class="title">ラソナの社内はこんなのよ</p>
-                        <p class="more"><a href="" class="line"><span class="readmore" href="http://">READ MORE</p>
-                        </span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="content-hover">
-                <div class="content">
-                    <img src="<?php echo get_template_directory_uri();?>/images/post_img_4.png">
-                    <div class="texts">
-                        <p class="day">2018/5/17</p>
-                        <p class="title">お隣のアラハはハワイ？</p>
-                        <p class="more"><a href="" class="line"><span class="readmore" href="http://">READ MORE</p>
-                        </span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="content-hover">
-                <div class="content">
-                    <img src="<?php echo get_template_directory_uri();?>/images/post_img_5.png">
-                    <div class="texts">
-                        <p class="day">2018/5/16</p>
-                        <p class="title">なぜテント？ラソナの人にきいてみた</p>
-                        <p class="more"><a href="" class="line"><span class="readmore" href="http://">READ MORE</p>
-                        </span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="content-hover">
-                <div class="content">
-                    <img src="<?php echo get_template_directory_uri();?>/images/post_img_6.png">
-                    <div class="texts">
-                        <p class="day">2018/5/15</p>
-                        <p class="title">ベイエリアおしゃれすぎる問題</p>
-                        <p class="more"><a href="" class="line"><span class="readmore" href="http://">READ MORE</p>
-                        </span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-<?php get_footer(); ?>
+
+            <?php get_footer(); ?>
